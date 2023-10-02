7f0962e718dcfd29ea0ac023c699a2f3ea12ffa4
stswoon <stswoon@users.noreply.github.com>
2023-10-02 09:39:38 +0400

    Change lingva.ml server

--- src/translate.js
+++ src/translate.js
@@ -3,7 +3,9 @@ const fetch = require('node-fetch');
 module.exports.translate = async function translate(text, fromLang, toLang) {
     try {
         console.log(`Translate from=${fromLang} to=${toLang}, text: \n${text.substring(0,50)}...`);
-        let url = `https://lingva.ml/api/v1/${fromLang}/${toLang}/${encodeURIComponent(text)}`;
+        //const domain = "https://lingva.ml";
+        const domain = "http://lingva-ml-standby.stswoon.ru";
+        let url = `${domain}/api/v1/${fromLang}/${toLang}/${encodeURIComponent(text)}`;
         let res = await fetch(url);
         res = await res.json();
         res = res.translation;
